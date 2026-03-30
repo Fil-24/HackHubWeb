c18763447138a1698d5b02ff48cf058120b81cfe
Filippo Fattori <135116064+Fil-24@users.noreply.github.com>
2026-03-30 17:14:46 +0200

    comunicazione front-back end
    risoluzione problematiche di comunicazione con api del backend

--- hackhub_frontend/src/app/app.config.ts
+++ hackhub_frontend/src/app/app.config.ts
@@ -1,11 +1,37 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
 import { routes } from './app.routes';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { AuthService } from './features/auth/service/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+
+    // Services
+    AuthService,
+    //AuthGuard,
+
+    // JWT Interceptor 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+
+    // Error Interceptor 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
   ]
 };
