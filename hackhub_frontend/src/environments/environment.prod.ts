31c13d3a959aaf438daae993104d6ea5fcb1a1fe
Filippo Fattori <135116064+Fil-24@users.noreply.github.com>
2026-05-14 21:46:26 +0200

    enviroment mod

--- hackhub_frontend/src/environments/environment.prod.ts
+++ hackhub_frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cn-hackhub-backend.internal.graysand-4ebad077.germanywestcentral.azurecontainerapps.io/api'
+  apiUrl: 'https://cn-hackhub-backend.graysand-4ebad077.germanywestcentral.azurecontainerapps.io/api'
 };
